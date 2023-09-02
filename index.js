c6d45cd850ba0600e1295b5bb80bf652c0528756
Mathzzuco <matheusmazzuco05@gmail.com>
2023-09-02 15:32:51 -0300

    finish playRound

--- index.js
+++ index.js
@@ -9,13 +9,32 @@ function getComputerChoice() {
     if (random == 2) {
         random = "Scissors"
     }
+    console.log(random)
     return random
 }
 
 function getPlayerChoice () {
     const choice = prompt("Choose")
+    console.log(choice)
     return choice
 }
 
+function playRound(lowercasePlayer, lowercaseComputer) {
+    if (lowercasePlayer == lowercaseComputer) {
+        return "Tie! " + playerSelection + " is equal to " + computerSelection
+    }
+    if (lowercasePlayer == "rock" && lowercaseComputer == "scissors" || lowercasePlayer == "paper" && lowercaseComputer == "rock" || lowercasePlayer == "scissors" && lowercaseComputer == "paper") {
+        return "You Win! " + playerSelection + " beats " + computerSelection
+    }
+    if (lowercaseComputer == "rock" && lowercasePlayer == "scissors" || lowercaseComputer == "paper" && lowercasePlayer == "rock" || lowercaseComputer == "scissors" && lowercasePlayer == "paper") {
+        return "You Lose! " + computerSelection + " beats " + playerSelection
+    }
+}
+
 const computerSelection = getComputerChoice()
+lowercaseComputer = computerSelection.toLowerCase()
+
 const playerSelection = getPlayerChoice()
+lowercasePlayer = playerSelection.toLowerCase()
+
+console.log(playRound(lowercasePlayer, lowercaseComputer))
